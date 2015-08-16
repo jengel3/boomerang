4c291f8d10c46901cd60626b631c6c259ed22624
Jake <jake0oo0dev@gmail.com>
2015-08-15 21:40:11 -0500

    init commit

---
+++ bin/boomerang-init.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var prompt = require('prompt');
+var fs = require("fs");
+
+var init = function() {
+  var args = process.argv.slice(2);
+  var dir;
+  if (args.length === 1) {
+    dir = args[0];
+    var exists = fs.existsSync(dir);
+    if (exists) {
+      console.error("Folder already exists");
+      return;
+    } else {
+      fs.mkdirSync(dir);
+    }
+  } else {
+    dir = ".";
+  }
+
+  prompt.start();
+
+  prompt.get(['domain'], function(err, result) {
+    fs.mkdirSync(dir + "/" + "depictions");
+    fs.mkdirSync(dir + "/" + "debs");
+    fs.writeFileSync(dir + "/CNAME", result.domain);
+    fs.writeFileSync(dir + "/README.md", "# Cydia Repo: " + result.domain);
+    fs.createReadStream(__dirname + "/../templates/add.js").pipe(fs.createWriteStream(dir + "/add.js"));
+  });
+}
+
+init();
+
+module.exports = init;
